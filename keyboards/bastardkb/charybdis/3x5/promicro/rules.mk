b61ca9bbf6e7118e3453fc81dc273b25bc14dc85
Nick Chow <nicky.chow@gmail.com>
2026-06-15 12:19:51 +1000

    add custom settings

---
+++ keyboards/bastardkb/charybdis/3x5/promicro/rules.mk
@@ -0,0 +1,7 @@
+AUDIO_SUPPORTED = no        # Audio is not supported
+RGB_MATRIX_SUPPORTED = yes  # RGB matrix is supported and enabled by default
+RGBLIGHT_SUPPORTED = yes    # RGB underglow is supported, but not enabled by default
+
+POINTING_DEVICE_DRIVER = pmw3389
+
+SERIAL_DRIVER = vendor
